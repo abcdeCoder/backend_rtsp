943584e7c15e00d565a8ab22c2624bb59bd73479
Gaurav verma <94301843+abcdeCoder@users.noreply.github.com>
2024-02-04 02:17:39 +0530

    updates

--- app.py
+++ app.py
@@ -93,5 +93,7 @@ def index():
     return "Overlay API is running!"
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__': 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
